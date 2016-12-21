f281c5fdf73020d37999f591eb5766d57e65f694
Vadim <vadimborisov1@gmail.com>
2016-12-21 13:20:39 +0100

    Update HelloCadence.py

--- HelloCadence.py
+++ HelloCadence.py
@@ -6,5 +6,5 @@ import os
 data = sys.stdin.readline() #read from SKILL
 data = data.replace('','')[:-2] #remove the \n
 sys.stdout.write('hello Cadence %s' % data ) # stdout for Cadence Virtuoso 
-sys.stdout.flush()	#flus
+sys.stdout.flush()	#flush
 sys.stdout.close()  # not sure about this line
