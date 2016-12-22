ea4a344781247663d9fc2c186854b1c2695d1164
Vadim <vadimborisov1@gmail.com>
2016-12-22 14:05:07 +0100

    Update HelloCadence.py
    pep8 correction

--- HelloCadence.py
+++ HelloCadence.py
@@ -1,10 +1,8 @@
 #!/usr/bin/python
-#shebang line
+# shebang line
 import sys
-import os
-
-data = sys.stdin.readline() #read from SKILL
-data = data.replace('','')[:-2] #remove the \n
-sys.stdout.write('hello Cadence %s' % data ) # stdout for Cadence Virtuoso 
-sys.stdout.flush()	#flush
+data = sys.stdin.readline()  # read from SKILL
+data = data.replace('', '')[:-2]  # remove the \n
+sys.stdout.write('hello Cadence %s' % data)  # stdout for Cadence Virtuoso
+sys.stdout.flush()  # flus
 sys.stdout.close()  # not sure about this line
